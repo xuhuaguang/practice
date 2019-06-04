b997b7d470b7edd14cbf0f73a76495a9c1a5b5e7
xuhuaguang <huaguang131400>
2019-06-04 19:11:39 +0800

    测试Java8

--- src/main/java/com/rxlm/practice/java8/Stream/TestListToMap.java
+++ src/main/java/com/rxlm/practice/java8/Stream/TestListToMap.java
@@ -4,8 +4,10 @@ import com.rxlm.practice.java8.bean.Orange;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 /**
@@ -27,7 +29,9 @@ public class TestListToMap {
         orangeList.add(orange2);
         orangeList.add(orange3);
         orangeList.add(orange4);
-        java8ListToMap(orangeList);
+        //java8ListToMap(orangeList);
+        //getIdNameMap(orangeList);
+        getNameOrangeMap(orangeList);
     }
 
     /**
@@ -36,9 +40,32 @@ public class TestListToMap {
      * toMap 如果集合对象有重复的key，会报错Duplicate key ....
      *  orange1,orange2的id都为1。
      *  可以用 (k1,k2)->k1 来设置，如果有重复的key,则保留key1,舍弃key2
+     *  key为对象的id,value为对象
      */
     public static void java8ListToMap(List<Orange> orangeList) {
-        Map<Integer, Orange> collect = orangeList.stream().collect(Collectors.toMap(Orange::getId, a -> a));
+        Map<Integer, Orange> collect = orangeList.stream().collect(Collectors.toMap(Orange::getId, a -> a,(k1,k2)->k1));
+        Map<Integer, Orange> collect1 = orangeList.stream().collect(Collectors.toMap(Orange::getId, Function.identity(),(k1, k2)->k1));
+        collect.forEach((k,v)->{
+            System.out.println(k+"------"+v.toString());
+        });
+    }
+
+    /**
+     *key为对象的id,value为对象的名称
+     * @param orangeList
+     * @return
+     */
+    public static void  getIdNameMap(List<Orange> orangeList) {
+        Map<Integer, String> collect = orangeList.stream().collect(Collectors.toMap(Orange::getId, Orange::getName,(key1, key2) -> key2));
+        collect.forEach((k,v)->{
+            System.out.println(k+"------"+v);
+        });
+    }
+
+    //指定具体收集的map
+    //比如我想获取LinkedHashMap
+    public static void getNameOrangeMap(List<Orange> orangeList) {
+        LinkedHashMap<Integer, Orange> collect = orangeList.stream().collect(Collectors.toMap(Orange::getId, Function.identity(), (k1, k2) -> k2, LinkedHashMap::new));
         collect.forEach((k,v)->{
             System.out.println(k+"------"+v.toString());
         });
